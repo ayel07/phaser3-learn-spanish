999fd50fa058c3ea46cb9d6c435454d11d228855
Marielle Monceller <marielle@monceller.com>
2021-10-28 00:10:58 +0800

    fix next question param error

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -173,7 +173,7 @@ export default class Demo extends Phaser.Scene {
     this.correctSound = this.sound.add('correct');
     this.wrongSound = this.sound.add('wrong');
 
-    this.showNextQuestion();
+    this.showNextQuestion(this.words[i].key);
   }
 
   processAnswer(userResponse: string) {
